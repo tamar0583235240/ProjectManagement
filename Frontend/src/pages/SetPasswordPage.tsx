9f7fcb5d4ec0c464d833ee878930d4d8588d7440
ayalacohen786 <ac33116878@gmail.com>
2025-05-26 13:00:21 +0300

    update

--- Frontend/src/pages/SetPasswordPage.tsx
+++ Frontend/src/pages/SetPasswordPage.tsx
@@ -1,25 +1,46 @@
-// // src/features/auth/pages/SetPasswordPage.tsx
-// import { useParams } from "react-router-dom";
-// import { SetPasswordForm } from "../components/SetPasswordForm";
+// // // src/features/auth/pages/SetPasswordPage.tsx
+// // import { useParams } from "react-router-dom";
+// // import { SetPasswordForm } from "../components/SetPasswordForm";
 
-// const SetPasswordPage = () => {
-//   const { token } = useParams<{ token: string }>();
+// // const SetPasswordPage = () => {
+// //   const { token } = useParams<{ token: string }>();
 
-//   if (!token) return <div>טוקן לא נמצא.</div>;
+// //   if (!token) return <div>טוקן לא נמצא.</div>;
 
-//   return <SetPasswordForm token={token} />;
-// };
+// //   return <SetPasswordForm token={token} />;
+// // };
+
+// // export default SetPasswordPage;
+
+
+// import { useSearchParams } from "react-router-dom";
+// import SetPasswordForm from "../components/SetPasswordForm";
+// import { Typography, Container } from "@mui/material";
+
+// const SetPasswordPage=()=> {
+//   const [params] = useSearchParams();
+//   const token = params.get("token");
+
+//   if (!token) {
+//     return <Typography color="error">קישור שגוי - אין טוקן.</Typography>;
+//   }
+
+//   return (
+//     <Container sx={{ mt: 8 }}>
+//       <SetPasswordForm token={token} />
+//     </Container>
+//   );
+// }
 
 // export default SetPasswordPage;
 
 
-import { useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import SetPasswordForm from "../components/SetPasswordForm";
 import { Typography, Container } from "@mui/material";
 
-const SetPasswordPage=()=> {
-  const [params] = useSearchParams();
-  const token = params.get("token");
+const SetPasswordPage = () => {
+  const { token } = useParams<{ token: string }>();
 
   if (!token) {
     return <Typography color="error">קישור שגוי - אין טוקן.</Typography>;
@@ -30,6 +51,6 @@ const SetPasswordPage=()=> {
       <SetPasswordForm token={token} />
     </Container>
   );
-}
+};
 
 export default SetPasswordPage;
