785e849413861c4dc410015fb965824ec9e8f3f4
ayalacohen786 <ac33116878@gmail.com>
2025-05-26 14:21:05 +0300

    update

--- Frontend/src/routes/router.tsx
+++ Frontend/src/routes/router.tsx
@@ -5,15 +5,12 @@ import Projects from "../pages/Projects";
 import Tasks from "../pages/Tasks";
 import EmployeeManagment from "../pages/EmployeeManagment";
 import NotFound from "../pages/NotFound";
-
 import LandingPage from "../pages/LandingPage";
 import InitialRouter from "../components/InitialRouter";
 import { Children } from "react";
 import SetPasswordPage from "../pages/SetPasswordPage";
 import AddUserForm from "../features/users/InviteUserForm";
 import InviteUserForm from "../features/users/InviteUserForm";
-
-
 const router = createBrowserRouter([
    {
     path: '/',
@@ -21,23 +18,18 @@ const router = createBrowserRouter([
   },
   {
     path: '/landingPage',
-    element: <LandingPage />
+    element: <LandingPage/>
   },
-
-
   {
     path: '/set-password/:token',
     element: <SetPasswordPage/>
   },
-
-
     {
         path: "app", element: <AppLayout />,
         children: [
             { element: <HomePage />, index: true },
             { path: "projects", element: <Projects /> },
             { path: "tasks", element: <Tasks /> },
-
             {
               path: "employeemanagement",
               element: <EmployeeManagment />,
@@ -55,5 +47,4 @@ const router = createBrowserRouter([
           ]
     },
 ]);
-
 export default router;
