69381152f40d8bc5d453b1c98937151e876b773d
ayalacohen786 <ac33116878@gmail.com>
2025-06-01 01:14:37 +0300

    update

--- Backend/controllers/inviteController.js
+++ Backend/controllers/inviteController.js
@@ -3,44 +3,6 @@ const generatePasswordToken = require('../utils/generateToken');
 const sendInviteEmail = require('../utils/mailer');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
-// // 1. יצירת משתמש חדש (הזמנה)
-// exports.inviteUser = async (req, res) => {
-//   try {
-//     const { email, role, manager_id, organization_id } = req.body;
-
-//     // בדיקה אם משתמש כבר קיים
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ message: 'User with this email already exists.' });
-//     }
-
-//     // יצירת טוקן חדש לבחירת סיסמה
-//     const password_token = generatePasswordToken();
-//     const password_token_expires = new Date(Date.now() + 48 * 60 * 60 * 1000);
-
-//     // יצירת משתמש חדש - סיסמה null, is_active=false
-//     const newUser = new User({
-//       email,
-//       user_name,
-//       role,
-//       manager_id: manager_id || null,
-//       organization_id,
-//       password_token,
-//       password_token_expires
-//     });
-
-//     const user = await User.create(newUser)
-
-//     // שליחת מייל עם טוקן
-//     await sendInviteEmail(email, password_token);
-
-//     res.status(201).json(user,{ message: 'Invitation sent successfully' });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
 exports.inviteUser = async (req, res) => {
   try {
     const { email, role, manager_id, organization_id } = req.body;
@@ -103,24 +65,8 @@ exports.setPassword = async (req, res) => {
       }
       return res.status(400).json({ message: 'Invalid or expired token' });
     }
-    
-    // const user = await User.findOne({
-    //   password_token: token,
-    //   password_token_expires: { $gt: new Date() }
-    // });
 
-    // if (!user) {
-    //   return res.status(400).json({ message: 'Invalid or expired token' });
-    // }
-
-    // הצפנת הסיסמה
-    // if (password) {
-    //   const hashedPwd = await bcrypt.hash(password, 10);
-    //   updateData.password = hashedPwd;
-    // }
     const hashedPassword = await bcrypt.hash(password, 10);
-
-
     
     // עדכון המשתמש: סיסמה, הפעלה, ניקוי הטוקן
     user.password = hashedPassword;
