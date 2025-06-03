3daf1ea3119bfb8ddce9e2a484994cb29aef0ef3
ayalacohen786 <ac33116878@gmail.com>
2025-06-03 16:38:02 +0300

    update

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User")
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const generatePasswordToken = require("../utils/generateToken");
 
 
 
