88b0eb016a972ea60c3adb41838c6d0c1853a23d
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-04 23:24:25 +0300

    remove verifyJWT

--- Backend/routes/usersRoutes.js
+++ Backend/routes/usersRoutes.js
@@ -7,8 +7,8 @@ const { getEmployeesByOrganization } = require('../controllers/usersController')
 
 // router.post('/signUp', signUp);
 // router.post('/AddUser', AddUser);
-router.delete('/DeleteUser/:user_id',verifyJWT, DeleteUser);
-router.put('/UpdateUser/:user_id',verifyJWT, UpdateUser);
+router.delete('/DeleteUser/:user_id', DeleteUser);
+router.put('/UpdateUser/:user_id', UpdateUser);
 // router.post('/validate-user',verifyJWT, validateUser);
 router.post('/validate-user', validateUser);
 // router.get('/getAllTeamMembersUnderManager/:managerId', verifyJWT, getAllTeamMembersUnderManager);
