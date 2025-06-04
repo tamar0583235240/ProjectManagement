db81cb3642193837d1dfa3f43e91278b3a94745d
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-04 04:37:35 +0300

    merge

--- Frontend/src/routes/router.tsx
+++ Frontend/src/routes/router.tsx
@@ -3,29 +3,19 @@ import AppLayout from "../components/AppLayout";
 import HomePage from "../pages/HomePage";
 import Projects from "../pages/Projects";
 import Tasks from "../pages/Tasks";
-import NotFound from "../pages/NotFound";
-<<<<<<< HEAD
 import LandingPage from "../pages/LandingPage";
 import InitialRouter from "../components/InitialRouter";
-import { Children } from "react";
 import SetPasswordPage from "../pages/SetPasswordPage";
-import AddUserForm from "../features/users/InviteUserForm";
 import InviteUserForm from "../features/users/InviteUserForm";
 import ResetPasswordPage from "../features/auth/ResetPasswordPage";
-=======
-import InitialRouter from "../components/InitialRouter";
-import LandingPage from "../pages/LandingPage";
-import ProjectDetails from "../features/Project/ProjectDetails";
 import EmployeeManagement from "../pages/EmployeeManagement";
 
->>>>>>> Frontend/Projects
 const router = createBrowserRouter([
   {
     path: '/',
     element: <InitialRouter />,
   },
   {
-<<<<<<< HEAD
     path: '/landingPage',
     element: <LandingPage/>
   },
@@ -44,12 +34,12 @@ const router = createBrowserRouter([
             { path: "projects", element: <Projects /> },
             { path: "tasks", element: <Tasks /> },
             {
-              path: "employeemanagement",
-              element: <EmployeeManagment />,
+              path: "employee-management",
+              element: <EmployeeManagement />,
               children: [
                 {
-                  index: true,  // ברירת מחדל בכניסה לנתיב הזה
-                  element: <InviteUserForm />  // במקום InitialRouter
+                  index: true, 
+                  element: <InviteUserForm /> 
                 },
                 {
                   path: "invite",
@@ -61,23 +51,3 @@ const router = createBrowserRouter([
     },
 ]);
 export default router;
-=======
-    path: '/landing',
-    element: <LandingPage />,
-  },
-  {
-    path: '/app',
-    element: <AppLayout />,
-    children: [
-      { index: true, element: <HomePage /> },
-      { path: 'projects', element: <Projects /> },
-       { path: 'projects/:projectId', element: <ProjectDetails /> },
-      { path: 'tasks', element: <Tasks /> },
-      { path: 'employee-management', element: <EmployeeManagement /> },
-      { path: '*', element: <NotFound /> },
-    ],
-  },
-]);
-
-export default router;
->>>>>>> Frontend/Projects
