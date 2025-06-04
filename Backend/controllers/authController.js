85f087eb8a6955be6809c3df029cf2dc21a4cbcd
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-05 00:14:51 +0300

    ...

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -52,7 +52,9 @@ exports.SignIn = async (req, res) => {
     }
 
     console.log(user);
-    
+    bcrypt.compare("214787673", "$2b$10$CvTyzzkXlhqXVE7fxdE7D.NXtTqKoABGsnabA1UftRxmv6oZ4mxWW")
+      .then(result => console.log("Password match:", result))
+
     const flag = await bcrypt.compare(password, user.password);
     console.log("Password match:", flag);
     const match = await bcrypt.compare(password, user.password);
