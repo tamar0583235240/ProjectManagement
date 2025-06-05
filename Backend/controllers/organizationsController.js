e4aa48c5cc85b134a9ab35d842842c1b5a676258
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-05 03:46:09 +0300

    ...

--- Backend/controllers/organizationsController.js
+++ Backend/controllers/organizationsController.js
@@ -50,3 +50,18 @@ exports.UpdateOrganization = async (req, res) => {
         res.status(500).json({ message: 'Failed to update organization' });
     }
 };
+ 
+exports.getOrganizationByUserId = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate('organization_id');
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    return res.status(200).json(user.organization_id);
+  } catch (error) {
+    console.error('Error fetching organization:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- Backend/routes/organizationsRoutes.js
+++ Backend/routes/organizationsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { AddOrganization, AllOrganization, DeleteOrganization, UpdateOrganization } = require('../controllers/organizationsController');
+const { AddOrganization, AllOrganization, DeleteOrganization, UpdateOrganization, getOrganizationByUserId } = require('../controllers/organizationsController');
 const verifyJWT = require('../middleware/verifyJWT');
 
 router.post('/AllOrganization',verifyJWT, AllOrganization);
 router.post('/AddOrganization',AddOrganization);
 router.delete('/DeleteOrganization/:organization_id',verifyJWT, DeleteOrganization);
 router.put('/UpdateOrganization/:organization_id',verifyJWT, UpdateOrganization);
+router.get('/getOrganizationByUserId/:userId', getOrganizationByUserId);
 
 module.exports = router;
