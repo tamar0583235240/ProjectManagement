283822e364c3b972f7fe96c84af9630deae43ac9
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-06 02:04:57 +0300

    update

--- Frontend/src/features/User/userApi.ts
+++ Frontend/src/features/User/userApi.ts
@@ -1,68 +1,4 @@
-// import { api } from "../../app/api";
-// import type { AddUserInputs } from "../../types/AddUserInputs";
-// import type { SetPasswordRequest } from "../../types/SetPasswordRequest";
-// import type { User } from "../../types/User";
 
-// export const userApi = api.injectEndpoints({
-//     endpoints: (builder) => ({
-//         validateUser: builder.mutation<{ _id: string } | null, { username: string; email: string }>({
-//             query: ({ username, email }) => ({
-//                 url: "Users/validate-user",
-//                 method: "POST",
-//                 body: { username, email },
-//             }),
-//         }),
-
-//         getTopManagerOfEmployee: builder.query<{ topManagerId: string }, string>({
-//             query: (employeeId) => ({
-//                 url: `Users/getTopManagerOfEmployee/${employeeId}`,
-//                 method: "GET",
-//             }),
-//         }),
-
-//         getEmployeesByOrganization: builder.query<{ _id: string; user_name: string; email: string }[], string>({
-//             query: (orgId) => ({
-//                 url: `Users/getEmployeesByOrg/${orgId}`,
-//                 method: "GET",
-//             }),
-//         }),
-//         getTeamLeaders: builder.query<User[], string>({
-//             query: (managerId) => ({
-//                 url: `user/getTeamLeaders/${managerId}`,
-//                 method: 'GET',
-//             }),
-//         }),
-//         inviteUser: builder.mutation<string, AddUserInputs>({
-//             query: (user) => ({
-//                 url: "invite/inviteUser",
-//                 method: "POST",
-//                 body: user,
-//             }),
-//             invalidatesTags: ["User"],
-//         }),
-//         setPassword: builder.mutation<string, SetPasswordRequest>({
-//             query: (body) => ({
-//                 url: "invite/setPassword",
-//                 method: 'POST',
-//                 body,
-//             }),
-//             invalidatesTags: ["User"],
-//         }),
-//     }),
-//     overrideExisting: false,
-// });
-
-// export const {
-//     useValidateUserMutation,
-//     useGetTopManagerOfEmployeeQuery,
-//     useGetEmployeesByOrganizationQuery,
-//     useGetTeamLeadersQuery,
-//     useInviteUserMutation,
-//     useSetPasswordMutation,
-// } = userApi;
-
-
-// src/features/User/userApi.ts
 import { api } from "../../app/api";
 import type { AddUserInputs } from "../../types/AddUserInputs";
 import type { SetPasswordRequest } from "../../types/SetPasswordRequest";
@@ -101,7 +37,6 @@ export const userApi = api.injectEndpoints({
       invalidatesTags: ['User'],
     }),
 
-    // עדכון משתמש
     updateUser: builder.mutation<User, { userId: string; data: Partial<User> }>({
       query: ({ userId, data }) => ({
         url: `/users/UpdateUser/${userId}`,
