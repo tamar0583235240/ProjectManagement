09e62d69bea86a21b6ee1ab358f5932ecd1805b3
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-10 02:08:28 +0300

    ...

--- Backend/config/corsOptions.js
+++ Backend/config/corsOptions.js
@@ -1,10 +1,22 @@
-var whitelist = ['http://localhost:5173/']
+// var whitelist = ['http://localhost:5173/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+  origin: [
+    'http://localhost:5173',
+    'https://projectmanagement-1-d01r.onrender.com'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }
+
+module.exports = corsOptions;
