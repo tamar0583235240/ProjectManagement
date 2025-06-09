507f546f00c92036a6a14b67da6d4b1a3be832dd
Tamar@0583235240 <tamar0583235240@gmail.com>
2025-06-10 02:32:26 +0300

    ...

--- Backend/app.js
+++ Backend/app.js
@@ -1,60 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const corsOptions = require("./config/corsOptions");
-// const connectDB = require("./config/dbConn");
-// const mongoose = require('mongoose');
-
-// const projectsRoutes = require('./routes/projectsRoutes');
-// const tasksRoutes = require('./routes/tasksRoutes');
-// const usersRoutes = require('./routes/usersRoutes');
-// const organizationsRoutes = require('./routes/organizationsRoutes');
-// const authRoutes = require('./routes/authRoutes');
-// const inviteRoutes = require('./routes/inviteRoutes');
-
-// const PORT = process.env.PORT || 7001;
-// const app = express();
-// connectDB();
-
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(express.static("public"));
-
-// app.use("/api/projects", projectsRoutes);
-// app.use("/api/tasks", tasksRoutes);
-// app.use("/api/users", usersRoutes);
-// app.use("/api/organizations", organizationsRoutes);
-// app.use("/api/auth",authRoutes )
-// app.use('/api/invite', inviteRoutes);
-
-// if (process.env.NODE_ENV !== 'production') {
-//   app.get("/", (req, res) => {
-//     res.send("this is the home page");
-//   });
-// }
-
-// const path = require('path');
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../Frontend/dist')));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
-//   });
-// }
-
-
-
-
-// mongoose.connection.once('open', () => {
-//     console.log('Connected to MongoDB');
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// });
-
-// mongoose.connection.on('error', err => {
-//     console.log(err);
-// });
-
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -91,7 +34,7 @@ app.use("/api/organizations", organizationsRoutes);
 app.use("/api/auth", authRoutes);
 app.use('/api/invite', inviteRoutes);
 
-app.get("/", (req, res) => {
+app.get("/app", (req, res) => {
   res.json({ message: "Backend API is running!" });
 });
 
